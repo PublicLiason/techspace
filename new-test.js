384b70176a00fadd2a073e72faff3c89f7ca7d8e
PublicLiason <101131912+PublicLiason@users.noreply.github.com>
2022-03-14 19:38:09 +0100

    logging out file data ...

--- new-test.js
+++ new-test.js
@@ -2,23 +2,4 @@ const file_data = require('./test-file.json')
 
 console.log(file_data)
 
-/*
-async function get() {
-    for (let i = 0; i < 110000; i++) {
-        console.log(`${file_data[i].id}`)
-    }
-}
 
-get()
-
-function somn() {
-    /* for (const stuff in file_data[0]) {
-        if (stuff === 'data' ) {
-            let {data} = file_data[0]
-            console.log(data)
-        }
-    } */
-    console.log(file_data[1].length)
-}
-somn()
-*/
