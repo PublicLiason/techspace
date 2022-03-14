4b54a2ce6455b68ef69d9ccee5e81351a6941885
PublicLiason <101131912+PublicLiason@users.noreply.github.com>
2022-03-14 19:16:43 +0100

    new test

---
+++ new-test.js
@@ -0,0 +1,20 @@
+const file_data = require('./test-file.json')
+
+async function get() {
+    for (let i = 0; i < 110000; i++) {
+        console.log(`${file_data[i].id}`)
+    }
+}
+
+get()
+
+function somn() {
+    /* for (const stuff in file_data[0]) {
+        if (stuff === 'data' ) {
+            let {data} = file_data[0]
+            console.log(data)
+        }
+    } */
+    console.log(file_data[1].length)
+}
+somn()
