352906fdd45298dfadf35823e6b3e411a1c26bbf
PublicLiason <101131912+PublicLiason@users.noreply.github.com>
2022-03-16 13:56:28 +0100

    deleting unnecessary files

--- add-file-test.js
+++
@@ -1,6 +0,0 @@
-const {createReadStream, createWriteStream} = require('fs')
-
-const stream_1 = createWriteStream('./test-file.json', {encoding: 'utf8', flags: 'a',})
-
-stream_1.on('error', error => console.log(error))
-module.exports = {stream_1}
